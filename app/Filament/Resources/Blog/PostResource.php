b581016ed3dbb88549d13e65b9e58d276790bddb
Robin Jay Alcantara <robin@mail.com>
2024-12-29 18:04:26 +0800

    Updates

---
+++ app/Filament/Resources/Blog/PostResource.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Filament\Resources\Blog;
+
+use App\Filament\Resources\Blog\PostResource\Pages;
+use App\Filament\Resources\Blog\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieTagsInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('title')
+                                    ->label('Title')
+                                    ->required()
+                                    ->live(onBlur: true)
+                                    ->placeholder('Enter the title of the post')
+                                    ->afterStateUpdated(fn (string $operation, $state, Forms\Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                                Forms\Components\TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required()
+                                    ->unique(Post::class, 'slug', ignoreRecord: true),
+
+                                Forms\Components\MarkdownEditor::make('content')
+                                    ->required()
+                                    ->columnSpan('full'),
+
+                                Forms\Components\DatePicker::make('published_at')
+                                    ->label('Published Date'),
+
+                                SpatieTagsInput::make('tags'),
+
+                            ])->columns(2),
+
+                        Forms\Components\Section::make('Image')
+                            ->schema([
+                                Forms\Components\FileUpload::make('image')
+                                    ->label('Image')
+                                    ->image()
+                                    ->hiddenLabel(),
+                            ])
+                            ->collapsible(),
+                    ])
+                    ->columnSpan(['lg' => fn (?Post $record) => $record === null ? 3 : 2]),
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Placeholder::make('created_at')
+                            ->label('Created at')
+                            ->content(fn (Post $record): ?string => $record->created_at?->diffForHumans()),
+
+                        Forms\Components\Placeholder::make('updated_at')
+                            ->label('Last modified at')
+                            ->content(fn (Post $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 1])
+                    ->hidden(fn (?Post $record) => $record === null),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image')
+                    ->label('Image'),
+
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Title'),
+
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Slug')
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                Tables\Columns\TextColumn::make('status')
+                    ->label('Status')
+                    ->getStateUsing(fn (Post $record) => $record->published_at?->isPast() ? 'Published' : 'Draft')
+                    ->badge(fn (Post $record) => $record->published_at?->isPast() ? 'success' : 'danger'),
+
+                Tables\Columns\TextColumn::make('published_at')
+                    ->label('Published Date')
+                    ->date(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+//            'create' => Pages\CreatePost::route('/create'),
+//            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    use HasTags;
+
+    protected $casts = [
+        'published_at' => 'date',
+    ];
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'published_at',
+        'seo_title',
+        'seo_description',
+        'image',
+    ];
+}
